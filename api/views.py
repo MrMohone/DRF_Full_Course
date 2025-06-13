a492b83e83232ee7c82784c2b8705d1c602f4421
MrMohone <mohammedawol0837@gmail.com>
2025-06-14 02:28:46 +0300

    function based serialization

--- api/views.py
+++ api/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
+@api_view(["GET"])# user can see only the data in the student table
 def studentsView(request):
-    students = Student.objects.all()
-    student_list = list(students.values())
-    return JsonResponse(student_list, safe=False)
+    if request.method == "GET":
+        #Get all the data the student table
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
