0d19d8868d6d50658ae307df462a1a27c6140b25
MrMohone <mohammedawol0837@gmail.com>
2025-06-14 03:40:27 +0300

    all function based serialized data commited

--- api/views.py
+++ api/views.py
@@ -6,10 +6,43 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(["GET"])# user can see only the data in the student table
+@api_view(["GET", "POST"])# user can see only the data in the student table
 def studentsView(request):
     if request.method == "GET":
         #Get all the data the student table
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        #Create a new student
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(["GET", "PUT", "DELETE"])# user can see, update and delete the data in the student table
+def studentsDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        #Get the data of a particular student
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == "PUT":
+        serializer = StudentSerializer(student, data=request.data)#new, and old data
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == "DELETE":
+        #Delete a particular student
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
